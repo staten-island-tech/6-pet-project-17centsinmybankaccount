247356376e912ba37ce4ddb0154ff990a1c3551f
17centsinmybankaccount <ansonc43@nycstudents.net>
2026-04-28 09:36:40 -0400

    Update christmas.py

--- christmas.py
+++ christmas.py
@@ -1,1 +1,11 @@
-class ca
+class Dog:
+    def _init_(self, name, breed):
+        self.name = name
+        self.breed = breed
+def bark(self):
+    print(f"{self.name} say Woof")
+def fetch(self,item):
+    print(f"{self.name} fetches the {item}"")
+my_dog = Dog("Buddy","Golden Retriever")
+print(my_dog.name)
+my_dog.bark()
